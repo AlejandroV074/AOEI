717a794ae8d6dd4595d3ab458e3af0a6f23aa24c
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-15 01:17:09 -0300

    style: se añade nueva imagen para el responsive

--- src/frontend/components/navbar/navbar.js
+++ src/frontend/components/navbar/navbar.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [showMenu, setShowMenu] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <nav>
@@ -18,7 +32,11 @@ function Navbar() {
             <ul className={`menu ${showMenu ? 'active' : ''}`}>
                 <li className="special-item">
                     <Link to="/" className="logo-label-container">
-                        <img src="/images/logo.png" alt="Logo" className="logo" />
+                        <img
+                            src={isSmallScreen ? "/images/logo-small.png" : "/images/logo.png"}
+                            alt="Logo"
+                            className="logo"
+                        />
                     </Link>
                 </li>
                 <li><Link to="/">Información</Link></li>
