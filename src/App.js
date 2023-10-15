b22057214e43eaf8c1b37eac99c39533d1d8c641
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-15 03:24:07 -0300

    add: se agrega componente info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './frontend/pages/home/home';
+import Info from './frontend/pages/info/info';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />} />
         </Routes>
       </Router>
     </div>
