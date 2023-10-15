97e6488e68ecab306483bdc1314741ba839cbc4a
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-14 23:59:55 -0300

    style: se agregan imagenes

---
+++ src/frontend/components/images-f/im.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './im.css';
+
+function Photos() {
+    return (
+
+        <div className='images-tittle'>
+            <img src="/images/celular.png" alt="celular" className="celular-image" />
+            <img src="/images/disk.png" alt="disk" className="disk-image" />
+            <img src="/images/pc.png" alt="pc" className="pc-image" />
+        </div>
+
+    );
+}
+
+export default Photos;
